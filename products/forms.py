cb32447c66c47ec1c434d045d3f16d667d4df78d
ergashfx2 <ergashaliabduvoxidov@gmail.com>
2023-11-24 13:41:25 +0500

    commits

--- products/forms.py
+++ products/forms.py
@@ -1,11 +1,17 @@
 from django import forms
+from django.utils.html import conditional_escape
 from django_summernote.widgets import SummernoteWidget
 
-from products.models import Product
+from products.models import Product, PRODUCT_TYPE_CHOICES
+
 
 
 class AddProduct(forms.ModelForm):
-    description = forms.CharField(widget=SummernoteWidget())
+    description = forms.CharField(label="Mahsulot haqida batafsil ma'lumot", widget=SummernoteWidget())
+    title = forms.CharField(label="Mahsulot nomi")
+    type = forms.ChoiceField(label="Mahsulot kategoriyasini tanlang", choices=PRODUCT_TYPE_CHOICES)
+    price = forms.IntegerField(label="Mahsulot narxi")
+    image = forms.ImageField(label="Mahsulot rasmini kiriting")
 
     class Meta:
         model = Product
@@ -13,7 +19,11 @@ class AddProduct(forms.ModelForm):
 
 
 class EditProduct(forms.ModelForm):
-    description = forms.CharField(widget=SummernoteWidget())
+    description = forms.CharField(label="Mahsulot haqida batafsil ma'lumot", widget=SummernoteWidget())
+    title = forms.CharField(label="Mahsulot nomi")
+    type = forms.ChoiceField(label="Mahsulot kategoriyasini tanlang", choices=PRODUCT_TYPE_CHOICES)
+    price = forms.IntegerField(label="Mahsulot narxi")
+    image = forms.ImageField(label="Mahsulot rasmini kiriting")
 
     class Meta:
         model = Product
