64600347bf545a9850f8c2c67fdc2972d1a064c9
ergashfx2 <ergashaliabduvoxidov@gmail.com>
2023-11-27 13:31:53 +0500

    commits

--- orders/forms.py
+++ orders/forms.py
@@ -5,7 +5,7 @@ class CreateOrder(forms.Form):
     name = forms.CharField(label="Ismingizni kiriting", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     phone = forms.CharField(label="Raqamingizni kiriting", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     location = forms.ChoiceField(label="Manzilingizni tanlang", choices=(
-        ('choice', 'Manzilingizni tanlash'),
+        ('Nomalum', 'Manzilingizni tanlash'),
         ('Toshkent Shahri', 'Toshkent Shahri'),
         ('Andijon Viloyati', 'Andijon Viloyati'),
         ('Buxoro Viloyati', 'Buxoro Viloyati'),
@@ -18,7 +18,7 @@ class CreateOrder(forms.Form):
         ('Surxondaryo Viloyati', "Surxondaryo Viloyati"),
         ('Toshkent Viloyati', "Toshkent Viloyati"),
         ('Xorazm Viloyati', "Xorazm Viloyati"),
-    ), widget=forms.Select(attrs={'class': 'form-select'}))
+    ),)
 
     def clean(self):
         cleaned_data = super().clean()
@@ -36,7 +36,7 @@ class EditOrderForm(forms.ModelForm):
         model = Order
         fields = ['customer_phone', 'customer_location', 'customer_name', 'status']
         widgets = {
-            'customer_phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'customer_phone': forms.NumberInput(attrs={'class': 'form-control'}),
             'customer_location': forms.Select(attrs={'class': 'form-select'}),
             'customer_name': forms.TextInput(attrs={'class': 'form-control'}),
             'status': forms.Select(attrs={'class': 'form-select'}),
@@ -45,9 +45,14 @@ class EditOrderForm(forms.ModelForm):
     def clean(self):
         cleaned_data = super().clean()
 
-        # Strip spaces from all fields
-        for field in self.fields:
-            if cleaned_data.get(field):
-                cleaned_data[field] = cleaned_data[field].strip()
+        # List of fields that should be treated as integers
+        integer_fields = ['customer_phone']
+
+        for field in integer_fields:
+            if field in cleaned_data:
+                try:
+                    cleaned_data[field] = int(cleaned_data[field])
+                except (ValueError, TypeError):
+                    self.add_error(field, 'Please enter a valid integer.')
 
         return cleaned_data
